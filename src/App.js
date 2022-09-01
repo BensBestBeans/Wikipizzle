e2d34615c6f68953f3c0e69ace95ae63dfa08fb1
Joel Richardson <joelwrichardson01@gmail.com>
2022-09-01 15:11:09 +1000

    nice

--- src/App.js
+++ src/App.js
@@ -4,8 +4,39 @@ import React from 'react';
 import ReactMarkdown from 'react-markdown'
 
 function getmd() {
-  return `# Wikipedia
-  So true!!!`
+  return `# Lau Theorem
+  From Wikipedia, the free encyclopedia
+  
+  
+  ## Introduction
+  
+  In mathematics, the Lau theorem, named after Danish mathematician Poul Lau, states that every countable subset of a separable metric space is itself separable.
+  
+  
+  ## Definition
+  
+  A separable metric space is a metric space in which every countable subset is itself a metric space.
+  
+  
+  ## Example
+  
+  The set of rational numbers is a separable metric space, since the subset of rational numbers consisting of the natural numbers is a metric space.
+  
+  
+  ## Proof
+  
+  The proof is by contradiction. Suppose that there exists a separable metric space that does not contain every countable subset. Let S be a countable subset of that space. Since S is separable, there exists a sequence in S converging to some point x in S. Since x is not in S, there must exist a point y in S such that x and y are not in the same metric space. But this is a contradiction, since the metric spaces consisting of the rational numbers and the points x, y are the same.
+  
+  
+  ## See also
+  
+   separable metric space
+  
+  
+  ## References
+  
+  [1] Poul Lau. "The Lau theorem." Bull. Amer. Math. Soc. 78 (1972): 1063-1064.
+  `
 }
 
 const md = getmd()
@@ -49,9 +80,12 @@ class App extends React.Component {
         </body>
 
         <div className='Ins'>
-        <button class="button1" role="button">WIKIPEDIA</button>
-        <button class="button2" role="button">AI GENERATED</button> 
-        
+        <div className='buttoncontainer'>
+        <button class="button1" role="button"><span>WIKIPEDIA</span></button>
+        </div>
+        <div className='buttoncontainer'>
+        <button class="button2" role="button"><span>AI GENERATED</span></button> 
+        </div>
         </div>
       </div>
     );

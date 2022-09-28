78394bac0ded589fa2a849927a8ef662e77a8f45
yeegoh <yeepinggoh@hotmail.com>
2022-09-28 13:33:43 +1000

    more tutorial page

--- src/App.js
+++ src/App.js
@@ -13,15 +13,15 @@ class App extends React.Component {
 
     console.log("BAKEND!");
 
-    fetch("http://localhost:3001/art")
-      .then((response) => response.text())
-      .then((data) => this.setState({ html: data }));
+    // fetch("http://localhost:3001/art")
+    //   .then((response) => response.text())
+    //   .then((data) => this.setState({ html: data }));
 
-    fetch("http://localhost:3001/type")
-      .then((response) => response.text())
-      .then((data) => {
-        this.mdtype = data == "w";
-      });
+    // fetch("http://localhost:3001/type")
+    //   .then((response) => response.text())
+    //   .then((data) => {
+    //     this.mdtype = data == "w";
+    //   });
 
     console.log("front!");
 
@@ -34,9 +34,9 @@ class App extends React.Component {
   }
 
   head = (
-    <h2 className="subtitle">
+    <p className="subtitle">
       Wikipedia or AI-Generated? Take a guess and test your skills!
-    </h2>
+    </p>
   );
 
   topbut = (mode, text) =>
@@ -99,7 +99,7 @@ class App extends React.Component {
 
   learn = () => (
     <div className="learn-container wrapper">
-      <div className="learn-intro">
+      <div className="learn-intro intro">
         <h1>What is Wikipizzle?</h1>
         <p>
           Wikipizzle is Lorem ipsum dolor sit amet, consectetur adipiscing elit.
@@ -115,13 +115,15 @@ class App extends React.Component {
           sit amet, consectetur adipiscing elit.{" "}
         </p>
       </div>
+
+
       <div className="">
         <h1>6 ways to spot a generated or fake article</h1>
       </div>
 
       <div className="learn-flexbox">
-        <div className="item"></div>
-        <div className="item">
+        <div className="learn-item"></div>
+        <div className="learn-item">
           <h2>1. Inconsistencies</h2>
           <p>
             The two main inconsistencies to look out for are facts that
@@ -129,7 +131,7 @@ class App extends React.Component {
             that do not support the content and lack correlation
           </p>
         </div>
-        <div className="item">
+        <div className="learn-item">
           <h2>2. Check the Vibes</h2>
           <p>Does the tone of writing match the topic it is discussing?</p>
           <p>
@@ -144,9 +146,9 @@ class App extends React.Component {
             then answering them?
           </p>
         </div>
-        <div className="item"></div>
-        <div className="item"></div>
-        <div className="item">
+        <div className="learn-item"></div>
+        <div className="learn-item"></div>
+        <div className="learn-item">
           <h2>3. Sloppy Writing</h2>
           <p>Poor grammar, spelling mistakes</p>
           <p>
@@ -154,7 +156,7 @@ class App extends React.Component {
             !!!, are a sign that the{" "}
           </p>
         </div>
-        <div className="item">
+        <div className="learn-item">
           <h2>4. Absence of quotes or sources</h2>
           <p>
             The presence of quotes and sources might add an additional layer of
@@ -163,128 +165,109 @@ class App extends React.Component {
             article.
           </p>
         </div>
-        <div className="item"></div>
-      </div>
-    </div>
-  );
-
-  tutorial = () => (
-    <div className="tutorial-container wrapper">
-      <div className="tutorial-item-left">
-        <div className="box-article box">
-          <h2>Joel's Hair</h2>
-          <p>
-            Joel's Hair is the name of an American rock band. The band was
-            formed in Omaha, Nebraska in 1992 by vocalist/guitarist Joel
-            Petersen and drummer Matt Bowen.{" "}
-            <span className="blue-highlight">
-              The band has released three albums; the latest, "The Speed of
-              Sound", in 2007
-            </span>
-            . The band's name was inspired by Petersen's long hair. In an
-            interview with the Omaha World-Herald, Petersen recalled, "I had
-            long hair at the time and the bass player [at the time] was like,
-            'Dude, you need a band named Joel's Hair.' I was like, 'All right,
-            that's a good band name.'"
-          </p>
-
-          <h2>Early Years</h2>
-          <p>
-            Joel's Hair began as a cover band in 1992, playing bars and clubs in
-            the Omaha area. In 1994, the band released its first album, "The
-            Joel's Hair EP", which was recorded live in the studio. The album
-            was well-received, and the band began to tour the Midwest.
-          </p>
-
-          <h2>The Cut</h2>
-          <p>
-            In 1995, the band released its second album, "The Cut". The album
-            was recorded in Chicago with producer Steve Albini, and was a
-            critical success, receiving favorable reviews from publications such
-            as Rolling Stone and Spin. The band continued to tour, and in 1997,
-            they released a live album, "Live at the Oasis".
-          </p>
-
-          <h2>Present Day</h2>
-          <p>
-            In{" "}
-            <span className="blue-highlight">
-              2000, the band released its third album, "The Speed of Sound"
-            </span>
-            . The album was recorded in Omaha, and was again well-received by
-            critics. In 2007, the band released a live DVD, "Live at the Waiting
-            Room".
-          </p>
-          <p>
-            The band is currently inactive, with Petersen focusing on his solo
-            project, The Petersen Effect.
-          </p>
-        </div>
-      </div>
-
-      <div className="tutorial-item-right">
-        <div className="box-one box">
-          <p>
-            Inconsistent dates are used to refer to the same album release date.
-            It says 2007 in the first section but later claims 2000.
-          </p>
-        </div>
-        <div className="box-two box">2</div>
-        <div className="box-three box">3</div>
+        <div className="learn-item"></div>
       </div>
     </div>
   );
 
   tutorial2 = () => (
     <div className="tutorial-container wrapper">
-      <div className="tutorial-item-left">
-        <div className="box-article box">
-          <p>
-            The University of Queensland (UQ) is a public research university
-            located in the city of Brisbane, the capital of the Australian state
-            of Queensland. Founded in 1901, UQ is colloquially known as{" "}
-            <span className="blue-highlight">the sandstone university</span>.
-          </p>
-          <p>
-            UQ is considered one of Australia's leading universities, and is
-            ranked as the 48th most reputable university in the world in the
-            2016 Times Higher Education World University Rankings. UQ is ranked
-            within the top 300 universities in thirteen indicators in the
-            2015-16 QS World University Rankings.
-          </p>
-          <p>
-            The main campus occupies much of the riverside inner suburb of St
-            Lucia, southwest of the Brisbane CBD. Other UQ campuses and
-            facilities are located throughout Queensland, the largest of which
-            are the Gatton campus and the herbarium at Mount Coot-tha. UQ also
-            has establishments overseas, such as the Brunei International School
-            in Brunei Darussalam.
-          </p>
-          <p>
-            Cras dictum orci interdum nibh laoreet, ac rutrum ipsum rutrum.
-            Aenean aliquam velit eu purus aliquet pretium. In ut ipsum ut justo
-            blandit tristique ac vel urna. Etiam molestie ligula sapien, nec
-            pharetra enim suscipit et. Praesent ultrices velit id ligula
-            lobortis consequat. Praesent id tortor lorem. Vivamus vestibulum
-            metus dui, at efficitur metus egestas quis. Nullam malesuada justo
-            enim, feugiat lobortis quam convallis ac. Nullam nec justo nunc.
-            Duis rutrum felis a ultrices viverra. In luctus accumsan turpis,
-            fermentum tincidunt erat auctor id. Mauris semper nunc quis massa
-            suscipit placerat. Integer vel semper urna.
-          </p>
+      <div className="tutorial-intro intro">
+        <h1>How did you go?</h1>
+          <div className="tutorial-guess">
+            <div className="tutorial-guess-left"><p>Incorrect</p></div>
+            <div className="tutorial-guess-right"><p>Deceived! Today's article was generated by AI.</p></div>
+          </div>
+        <p>How might you be able to tell? Take a look below on what you may have missed.</p>
+      </div>
+
+      <div className="tutorial-main">
+        <div className="tutorial-item-left">
+          <div className="box-article box">
+            <h2>Joel's Hair</h2>
+            <p>Joel's Hair is the name of an American rock band. The band was formed in Omaha, Nebraska in 1992 by vocalist/guitarist Joel Petersen and drummer Matt Bowen.{" "} <span className="blue-highlight"> The band has released three albums; the latest, "The Speed of Sound", in 2007</span>. The band's name was inspired by Petersen's long hair. In an interview with the Omaha World-Herald, Petersen recalled, "I had long hair at the time and the bass player [at the time] was like, 'Dude, you need a band named Joel's Hair.' I was like, 'All right, that's a good band name.'"</p>
+            <h2>Early Years</h2>
+            <p>Joel's Hair began as a cover band in 1992, playing bars and clubs in the Omaha area. In 1994, the band released its first album, "The Joel's Hair EP", which was recorded live in the studio. The album was well-received, and the band began to tour the Midwest.</p>
+            <h2>The Cut</h2>
+            <p>In 1995, the band released its second album, "The Cut". The album was recorded in Chicago with producer Steve Albini, and was a critical success, receiving favorable reviews from publications such as Rolling Stone and Spin. The band continued to tour, and in 1997, they released a live album, "Live at the Oasis".</p>
+            <h2>Present Day</h2>
+            <p>In{" "}<span className="blue-highlight">2000, the band released its third album, "The Speed of Sound"</span>. The album was in Omaha, and was again well-received by critics. In 2007, the band released a live DVD, "Live at the Waiting Room".</p>
+            <p>The band is currently inactive, with Petersen focusing on his solo project, The Petersen Effect.</p>
+          </div>
+        </div>
+
+        <div className="tutorial-item-right">
+          <div className="blue-box box">
+            <p>Inconsistent dates are used to refer to the same album release date. It says 2007 in the first section but later claims 2000.</p>
+          </div>
+          <div className="pink-box box">2</div>
+          <div className="green-box box">3</div>
         </div>
       </div>
 
-      <div className="tutorial-item-right">
-        <div className="box-one box">
-          <p>
-            The article should have used title case for "The Sandstone
-            University" when referring and proper nouns.
-          </p>
-        </div>
-        <div className="box-two box">2</div>
-        <div className="box-three box">3</div>
+
+    </div>
+  );
+
+  tutorial = () => (
+    <div className="tutorial-container wrapper">
+      <div className="tutorial-intro intro">
+        <h1>Tutorial 2/3</h1>
+        <p></p>
       </div>
+
+      <div className="tutorial-main">
+        <div className="tutorial-item-left">
+          <div className="box-article box">
+            <h2>Introduction</h2>
+            <p>
+              The University of Queensland (UQ) is a public research university
+              located in the city of Brisbane, the capital of the Australian state
+              of Queensland. Founded in 1901, UQ is colloquially known as{" "}
+              <span className="blue-highlight">the sandstone university</span>.
+            </p>
+            <p>
+              UQ is considered one of Australia's leading universities, and is
+              ranked as the 48th most reputable university in the world in the
+              2016 Times Higher Education World University Rankings. UQ is ranked
+              within the top 300 universities in thirteen indicators in the
+              2015-16 QS World University Rankings.
+            </p>
+            <h2>Campuses and Facilities</h2>
+            <p>The main campus occupies much of the riverside inner suburb of St
+              Lucia, southwest of the Brisbane CBD. Other UQ campuses and
+              facilities are located throughout Queensland, the largest of which
+              are the Gatton campus and the herbarium at Mount Coot-tha. UQ also
+              has establishments overseas, such as the Brunei International School
+              in Brunei Darussalam.
+            </p>
+            <p>
+              Cras dictum orci interdum nibh laoreet, ac rutrum ipsum rutrum.
+              Aenean aliquam velit eu purus aliquet pretium. In ut ipsum ut justo
+              blandit tristique ac vel urna. Etiam molestie ligula sapien, nec
+              pharetra enim suscipit et. Praesent ultrices velit id ligula
+              lobortis consequat. Praesent id tortor lorem. Vivamus vestibulum
+              metus dui, at efficitur metus egestas quis. Nullam malesuada justo
+              enim, feugiat lobortis quam convallis ac. Nullam nec justo nunc.
+              Duis rutrum felis a ultrices viverra. In luctus accumsan turpis,
+              fermentum tincidunt erat auctor id. Mauris semper nunc quis massa
+              suscipit placerat. Integer vel semper urna.
+            </p>
+          </div>
+        </div>
+
+        <div className="tutorial-item-right">
+          <div className="blue-box box">
+            <p>
+              The article should have used title case for "The Sandstone
+              University" when referring and proper nouns.
+            </p>
+          </div>
+          <div className="pink-box box">2</div>
+          <div className="green-box box">3</div>
+        </div>
+      </div>
+
     </div>
   );
 
@@ -464,7 +447,7 @@ class App extends React.Component {
         return (
           <div className="page-container-tutorial" id="tutorial">
             {" "}
-            {this.tutorial()}{" "}
+            {this.tutorial2()}{" "}
           </div>
         );
       default:
